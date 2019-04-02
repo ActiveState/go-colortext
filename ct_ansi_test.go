b536c01effdd4170bc7f9078cb6e8a14e932f1d4
Nathan Rijksen <n.rijksen@gmail.com>
2019-04-02 12:09:19 -0400

    Use testify instead of golangplus/testing

--- ct_ansi_test.go
+++ ct_ansi_test.go
@@ -5,14 +5,14 @@ package ct
 import (
 	"testing"
 
-	"github.com/golangplus/testing/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAnsiText(t *testing.T) {
-	assert.Equal(t, "ansiText", ansiText(None, false, None, false), "")
-	assert.Equal(t, "ansiText", ansiText(Red, false, None, false), "\x1b[0;31m")
-	assert.Equal(t, "ansiText", ansiText(Red, true, None, false), "\x1b[0;31;1m")
-	assert.Equal(t, "ansiText", ansiText(None, false, Green, false), "\x1b[0;42m")
-	assert.Equal(t, "ansiText", ansiText(Red, false, Green, false), "\x1b[0;31;42m")
-	assert.Equal(t, "ansiText", ansiText(Red, true, Green, false), "\x1b[0;31;1;42m")
+	assert.Equal(t, "", ansiText(None, false, None, false))
+	assert.Equal(t, "\x1b[0;31m", ansiText(Red, false, None, false))
+	assert.Equal(t, "\x1b[0;31;1m", ansiText(Red, true, None, false))
+	assert.Equal(t, "\x1b[0;42m", ansiText(None, false, Green, false))
+	assert.Equal(t, "\x1b[0;31;42m", ansiText(Red, false, Green, false))
+	assert.Equal(t, "\x1b[0;31;1;42m", ansiText(Red, true, Green, false))
 }
